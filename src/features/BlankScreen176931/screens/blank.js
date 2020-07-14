d6f0389b779e873697389aae6fe8af01c151185f
Crowdbotics <team@crowdbotics.com>
2020-07-14 16:29:06 +0000

    Update components

--- src/features/BlankScreen176931/screens/blank.js
+++ src/features/BlankScreen176931/screens/blank.js
@@ -132,7 +132,7 @@ export class _Blank extends React.Component {
           <View
             style={{
               width: "100%",
-              height: "30%",
+              height: "20%",
               marginLeft: 0,
               marginRight: 0,
               marginTop: 0,
@@ -236,10 +236,41 @@ export class _Blank extends React.Component {
                   borderRadius: 0,
                   backgroundSize: "auto"
                 }}
-              />
+              >
+                <Image
+                  resizeMode="contain"
+                  source={{
+                    uri:
+                      "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/profile_pic_5cPAZ4p.png"
+                  }}
+                  style={{
+                    width: "100%",
+                    height: 130,
+                    marginLeft: 0,
+                    marginRight: 0,
+                    marginTop: 0,
+                    marginBottom: 0,
+                    paddingLeft: 0,
+                    paddingRight: 0,
+                    paddingTop: 0,
+                    paddingBottom: 0,
+                    overflow: "visible",
+                    textAlign: "left",
+                    verticalAlign: "baseline",
+                    borderColor: "#000000",
+                    borderStyle: "solid",
+                    borderWidth: 0,
+                    borderLeftWidth: 0,
+                    borderRightWidth: 0,
+                    borderTopWidth: 0,
+                    borderBottomWidth: 0,
+                    borderRadius: 0
+                  }}
+                />
+              </View>
               <View
                 style={{
-                  width: "50%",
+                  width: "70%",
                   height: "100%",
                   marginLeft: 0,
                   marginRight: 0,
@@ -272,7 +303,117 @@ export class _Blank extends React.Component {
                   borderRadius: 0,
                   backgroundSize: "auto"
                 }}
-              />
+              >
+                <View
+                  style={{
+                    width: "100%",
+                    height: "50%",
+                    marginLeft: 0,
+                    marginRight: 0,
+                    marginTop: 0,
+                    marginBottom: 0,
+                    paddingLeft: 0,
+                    paddingRight: 0,
+                    paddingTop: 0,
+                    paddingBottom: 0,
+                    overflow: "visible",
+                    textAlign: "left",
+                    verticalAlign: "baseline",
+                    fontSize: 20,
+                    color: "#000000",
+                    backgroundColor: "#ffffff",
+                    fontStyle: "normal",
+                    fontWeight: "normal",
+                    flexDirection: "column",
+                    flexWrap: "wrap",
+                    justifyContent: "flex-start",
+                    alignItems: "stretch",
+                    alignContent: "stretch",
+                    borderColor: "#000000",
+                    borderStyle: "solid",
+                    borderWidth: 0,
+                    borderLeftWidth: 0,
+                    borderRightWidth: 0,
+                    borderTopWidth: 0,
+                    borderBottomWidth: 0,
+                    borderRadius: 0,
+                    backgroundSize: "auto"
+                  }}
+                >
+                  <View
+                    style={{
+                      width: "100%",
+                      height: "60%",
+                      marginLeft: 0,
+                      marginRight: 0,
+                      marginTop: 0,
+                      marginBottom: 0,
+                      paddingLeft: 0,
+                      paddingRight: 0,
+                      paddingTop: 0,
+                      paddingBottom: 0,
+                      overflow: "visible",
+                      textAlign: "left",
+                      verticalAlign: "baseline",
+                      fontSize: 20,
+                      color: "#000000",
+                      backgroundColor: "#ffffff",
+                      fontStyle: "normal",
+                      fontWeight: "normal",
+                      flexDirection: "column",
+                      flexWrap: "wrap",
+                      justifyContent: "flex-start",
+                      alignItems: "stretch",
+                      alignContent: "stretch",
+                      borderColor: "#000000",
+                      borderStyle: "solid",
+                      borderWidth: 0,
+                      borderLeftWidth: 0,
+                      borderRightWidth: 0,
+                      borderTopWidth: 0,
+                      borderBottomWidth: 0,
+                      borderRadius: 0,
+                      backgroundSize: "auto"
+                    }}
+                  />
+                  <View
+                    style={{
+                      width: "100%",
+                      height: "40%",
+                      marginLeft: 0,
+                      marginRight: 0,
+                      marginTop: 0,
+                      marginBottom: 0,
+                      paddingLeft: 0,
+                      paddingRight: 0,
+                      paddingTop: 0,
+                      paddingBottom: 0,
+                      overflow: "visible",
+                      textAlign: "left",
+                      verticalAlign: "baseline",
+                      fontSize: 20,
+                      color: "#000000",
+                      backgroundColor: "#ffffff",
+                      fontStyle: "normal",
+                      fontWeight: "normal",
+                      flexDirection: "column",
+                      flexWrap: "wrap",
+                      justifyContent: "flex-start",
+                      alignItems: "stretch",
+                      alignContent: "stretch",
+                      borderColor: "#000000",
+                      borderStyle: "solid",
+                      borderWidth: 0,
+                      borderLeftWidth: 0,
+                      borderRightWidth: 0,
+                      borderTopWidth: 0,
+                      borderBottomWidth: 0,
+                      borderRadius: 0,
+                      backgroundSize: "auto"
+                    }}
+                  />
+                </View>
+              </View>
             </View>
           </View>
           <View
@@ -314,7 +455,7 @@ export class _Blank extends React.Component {
           <View
             style={{
               width: "100%",
-              height: "50%",
+              height: "60%",
               marginLeft: 0,
               marginRight: 0,
               marginTop: 0,
